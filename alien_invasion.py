6cefa1aafb15fa4a9addab41481a7f12a9eb0eaa
ankit-poddar <ankitpoddar321@gmail.com>
2024-05-12 14:26:30 +0530

    Removed sys library from alien_invasion.py, it only runs in game_functions.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,6 @@
 import pygame
 from settings import Settings
 from ship import Ship
-import sys
 import game_functions as gf
 def run_game():
     #initialize game and create a screen object
@@ -18,14 +17,9 @@ def run_game():
 
     #start the main loop for the game
     while True:
+        gf.check_events()
+        gf.update_screen(ai_settings,screen,ship)
 
-        #watch for keyboard or mouse event
-        for event in pygame.event.get():
-            screen.fill(ai_settings.bg_color)
-            ship.blitme()
-            if event.type == pygame.QUIT:
-                sys.exit()
-            # make the most recently screen visible
-            pygame.display.flip()
+
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -6,3 +6,13 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings,screen,ship):
+    """Update images to the screen and flip to the new screen"""
+    #Redraw the screen with each pass through loop
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    #make the most recently screen visible
+    pygame.display.flip()
+
