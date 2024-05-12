a77cbc5dc2ad5ffe559c5bcc5e897eb5167d973d
ankit-poddar <ankitpoddar321@gmail.com>
2024-05-12 14:17:26 +0530

    Refactoring project, added game_functions.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,19 @@
 import pygame
 from settings import Settings
+from ship import Ship
 import sys
-
+import game_functions as gf
 def run_game():
     #initialize game and create a screen object
     pygame.init()
     ai_settings = Settings()
 
+
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption('Alien_invasion')
 
+    #Make a ship
+    ship = Ship(screen)
 
 
     #start the main loop for the game
@@ -18,6 +22,7 @@ def run_game():
         #watch for keyboard or mouse event
         for event in pygame.event.get():
             screen.fill(ai_settings.bg_color)
+            ship.blitme()
             if event.type == pygame.QUIT:
                 sys.exit()
             # make the most recently screen visible
--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ class Settings():
     def __init__(self):
         """initialize the game setting"""
         #Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230,230,230)
