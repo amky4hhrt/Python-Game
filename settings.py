22053e64a653a7b872edde011800049f3f8116bc
ankit-poddar <ankitpoddar321@gmail.com>
2024-05-12 12:31:29 +0530

    updated settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+#Setting and config file
+
+class Settings():
+    """A class to store all settings for alien invasion"""
+
+    def __init__(self):
+        """initialize the game setting"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
